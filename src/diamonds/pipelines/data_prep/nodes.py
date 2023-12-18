44efe8e54b5b08176336f275005b50285d2bfcc8
pustelnikk <s22674@pjwstk.edu.pl>
2023-12-18 20:48:41 +0100

    Updated nodes, pipeline and catalog

--- src/diamonds/pipelines/data_prep/nodes.py
+++ src/diamonds/pipelines/data_prep/nodes.py
@@ -4,28 +4,70 @@ generated using Kedro 0.18.14
 """
 
 import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
-from kedro.extras.datasets.pandas import CSVDataSet
-
+from sklearn.preprocessing import LabelEncoder
+from typing import Tuple
+from sklearn.model_selection import  train_test_split
 def remove_index(data: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Removes index column from raw .csv datasets
+    Args:
+        data: raw dataset
+
+    Returns: dataset without an index column
+
+    '''
     data = data.reset_index(drop=True)
     data = data.iloc[:, 1:]
     return data
 
-def onehot(data: pd.DataFrame) -> pd.DataFrame:
-    cat_col_names = data.select_dtypes(include=['object']).columns.tolist()
-    hot = OneHotEncoder()
-    hot = hot.fit(data[cat_col_names].copy())
-    encoded = hot.transform(data[cat_col_names].copy())
-    column_names = hot.get_feature_names_out()
-    hot_pd = pd.DataFrame(encoded.toarray(), columns=column_names)
-    data = data.reset_index(drop=True)
-    data = data.drop(cat_col_names, axis=1)
-    data_con = pd.concat([data, hot_pd], axis=1)
-    return data_con
+def remove_outliers(data: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Removes outlier records for selected numerical columns
+    Args:
+        data: input dataset
 
-def split_data():   
-    pass
+    Returns: dataset without outliers
+
+    '''
+    num_cols = ['x','y','z','depth','carat']
+    for col_name in num_cols:
+        q1 = data[col_name].quantile(0.25)
+        q3 = data[col_name].quantile(0.75)
+        iqr = q3 - q1
+        cond = (data[col_name] <= q3 + 1.5 * iqr) & (data[col_name] >= q1 - 1.5 * iqr)
+        data = data[cond]
+
+    return data
+
+
+def encode_labels(data: pd.DataFrame) -> Tuple[pd.DataFrame, LabelEncoder]:
+    '''
+    Encodes categorical features with LabelEncoder
+    Args:
+        data: input dataframe
+
+    Returns: encoded dataset, encoder
+
+    '''
+    cat_col_names = data.select_dtypes(include=['object']).columns.tolist()
+    encoder = LabelEncoder()
+    for col in cat_col_names:
+        data[col] = encoder.fit_transform(data[col])
+    return data, encoder
+
+def split_data(data: pd.DataFrame) -> Tuple:
+    '''
+    Loads enocded data and split it using train_test_split
+    Args:
+        data: input data
+
+    Returns: X_train, X_test, y_train, y_test - standard convetion
+    '''
+    price = data.price
+    data = data.drop(['price'],axis=1)
+    X_train, X_test, y_train, y_test = train_test_split(data, price, test_size=0.2, random_state=1)
+
+    return X_train, X_test, y_train, y_test
 def standardize_train():
     pass
 def standardize_test():
--- src/diamonds/pipelines/data_prep/pipeline.py
+++ src/diamonds/pipelines/data_prep/pipeline.py
@@ -3,17 +3,26 @@ This is a boilerplate pipeline 'data_prep'
 generated using Kedro 0.18.14
 """
 from kedro.pipeline import Pipeline, pipeline, node
-from .nodes import remove_index,onehot
+from .nodes import remove_index, remove_outliers, encode_labels, split_data
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
         node(func=remove_index,
-             inputs='data1',
-             outputs='data2',
-             name="remove_index_noed"),
-        node(func=onehot,
-             inputs='data2',
-             outputs='onehot_node',
-             name='onehot_node')
+             inputs='diamonds',
+             outputs='index_removed',
+             name="remove_index_node"),
+
+        node(func=remove_outliers,
+             inputs='index_removed',
+             outputs='outliers_removed',
+             name='remove_outliers_node'),
+        node(func=encode_labels,
+             inputs='outliers_removed',
+             outputs=['labels_encoded','label_encoder'],
+             name='encode_labels_node'),
+        node(func=split_data,
+             inputs='labels_encoded',
+             outputs=['X_train','X_test','y_train','y_test'],
+             name='split_data_node'),
     ])
